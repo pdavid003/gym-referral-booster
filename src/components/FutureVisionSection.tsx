c491c5f398199d8f25586228624c727756db9fe0
pdavid003 <p.david@twostepfit.com>
2025-07-23 15:56:14 +0200

    Updated FutureVisionSection.tsx

--- src/components/FutureVisionSection.tsx
+++ src/components/FutureVisionSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FutureVisionSection: React.FC = () => {
   return (
-    <section id="future-vision" className="py-20 bg-gray-900 text-white">
+    <section id="future-vision" className="py-20 bg-gray-600 text-white">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto">
           <div className="space-y-6">
